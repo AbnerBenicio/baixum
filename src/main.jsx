baf642be1696caf81046236e1120610f61406949
Abner Benicio <abnerbeniciosilva123@gmail.com>
2024-04-22 12:34:29 -0300

    feat: configurando rotas

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,47 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
+
+import Login from './pages/Login.jsx'
+import User from './pages/User.jsx'
+import Profile from './pages/Profile.jsx'
+import Home from './pages/Home.jsx'
+import Register from './pages/Register.jsx'
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <App />,
+    children: [
+      {
+        path:"/",
+        element:<Login />
+      },
+      {
+        path:"/register",
+        element:<Register />
+      },
+      {
+        path:"/:id/user",
+        element:<User />,
+        children: [
+          {
+            path: "/:id/user",
+            element: <Home />
+          },
+          {
+            path: "/:id/user/profile",
+            element: <Profile />
+          },
+        ]
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )

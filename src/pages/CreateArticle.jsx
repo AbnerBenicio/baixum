b171de3ae1852327d3452092b0f5a11a678d5993
Abner Benicio <abnerbeniciosilva123@gmail.com>
2024-04-23 14:35:19 -0300

    feat: criando formulário para adicao de artigo

--- src/pages/CreateArticle.jsx
+++ src/pages/CreateArticle.jsx
@@ -1,9 +1,113 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import API from "../api/api";
 
+const CreateArticle = () => {
+  //Criando variáveis para guardar dados do fomulário e autor
+  const [titulo, setTitulo] = useState("");
+  const [tema, setTema] = useState("");
+  const [conteudo, setConteudo] = useState("");
+  const [autor, setAutor] = useState("");
+  const { id } = useParams();
 
-function CreateArticle() {
+  //Buscando autor/usuário
+  useEffect(() => {
+    const fetchApi = async () => {
+      const res = await API.get(`user/${id}`);
+      setAutor(res.data);
+    };
+
+    fetchApi();
+  }, [id]);
+
+  //Função para limpeza das varáveis
+  const cleanData = () => {
+    setTitulo("");
+    setTema("");
+    setConteudo("");
+  };
+
+  //Função para submeter o formulário
+  const handleSubmit = async (e) => {
+    //Evitando atualização da página
+    e.preventDefault();
+
+    //Verificando se campos estão preenchidos
+    if (titulo !== "" && tema !== "" && conteudo !== "") {
+      //Definindo dados do artigo
+      const artigo = {
+        titulo: titulo,
+        conteudo: conteudo,
+        autor: autor.name,
+        tema: tema,
+      };
+
+      //Tentando enviar formulário
+      try {
+        //Enviando artigo
+        await API.post("/articles", artigo);
+        //Alertando ao usuário que artigo foi enviado com sucesso
+        alert("Artigo enviado com sucesso!");
+        // Limpando os campos do formulário
+        cleanData();
+      } catch (error) {
+        //Alertando erro de envio, se houver
+        alert("Erro ao enviar o artigo: " + error);
+      }
+    } else {
+      //Alertando que existem campos não preenchidos
+      alert("Todas as informações devem ser preenchidas");
+    }
+  };
+
+  //Retornando página
   return (
-    <div>CreateArticle</div>
-  )
-}
+    <div className="add-artigo">
+      {/*Formulário de criação do artigo*/}
+      <form onSubmit={handleSubmit}>
+        {/*Campo para título do artigo*/}
+        <label>
+          <span>Título</span>
+          <input
+            value={titulo}
+            type="text"
+            name="titulo"
+            onChange={(e) => setTitulo(e.target.value)}
+          />
+        </label>
+        {/*Campo para tema do artigo*/}
+        <label>
+          <span>Tema</span>
+          <select
+            value={tema}
+            name="tema"
+            onChange={(e) => setTema(e.target.value)}
+          >
+            <option value="">Selecione...</option>
+            <option value="programacao">Programação</option>
+            <option value="auto-ajuda">Auto-ajuda</option>
+          </select>
+        </label>
+        {/*Campo para conteúdo do artigo*/}
+        <label>
+          <label>Conteúdo</label>
+          <textarea
+            value={conteudo}
+            name="conteudo"
+            id=""
+            cols="60"
+            rows="10"
+            style={{ resize: "none" }}
+            onChange={(e) => setConteudo(e.target.value)}
+          ></textarea>
+        </label>
 
-export default CreateArticle
+        {/*Botão para envio do formulário*/}
+        <button>Enviar</button>
+      </form>
+    </div>
+  );
+};
+
+//Exportando página
+export default CreateArticle;

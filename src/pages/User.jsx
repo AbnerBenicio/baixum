3ac3a218e23a745a70b65376f0288f3373645ca1
ArthurCremasco <arthurcremascoamaral7@gmail.com>
2024-05-04 12:04:53 -0300

    Navbar

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -22,28 +22,28 @@ const User = () => {
   //Retornando página
   return (
     <div className="user-container">
-        {/*Navbar do usuário*/}
-        <nav>
-          {/*Saudação ao usuário*/}
-            <h1 className="greeting">Olá, {usuario ? usuario.name : ''}</h1>
-            {/*Links de navegação*/}
-            <div className="selection-buttons">
-                <Link to={`/${usuarioID}/user`} >Home</Link> {/*Link para home*/}
-                <Link to={`/${usuarioID}/user/profile`} >Perfil</Link> {/*Link para perfil*/}
-                <Link to={`/${usuarioID}/user/adicionar-artigos`} >Adicionar Artigo</Link> {/*Link para pagina de adicionar artigo*/}
-                <Link to={`/${usuarioID}/user/artigos`} >Artigos</Link> {/*Link para artigos*/}
-                <Link to={`/${usuarioID}/user/meus-artigos`} >Meus Artigos</Link> {/*Link para meus artigos*/}
-            </div>
-        </nav>
-        
-        {/*Conteúdo principal*/}
-        <main className="main-content">
-            <Outlet /> {/*Exibindo conteúdo das páginas filhas de User*/}
-        </main>
-
+      {/*Navbar do usuário*/}
+      <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+        {/*Links de navegação*/}
+        <div className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+          <Link to={`/${usuarioID}/user`} className="nav-link">Home</Link> {/*Link para home*/}
+          <Link to={`/${usuarioID}/user/profile`} className="nav-link">Perfil</Link> {/*Link para perfil*/}
+          <Link to={`/${usuarioID}/user/adicionar-artigos`} className="nav-link">Adicionar Artigo</Link> {/*Link para pagina de adicionar artigo*/}
+          <Link to={`/${usuarioID}/user/artigos`} className="nav-link">Artigos</Link> {/*Link para artigos*/}
+          <Link to={`/${usuarioID}/user/meus-artigos`} className="nav-link">Meus Artigos</Link> {/*Link para meus artigos*/}
+        </div>
+        {/*Saudação ao usuário*/}
+        <h5 className="greeting">Olá, {usuario ? usuario.name : ''}</h5>
+      </nav>
+      
+      {/*Conteúdo principal*/}
+      <main className="main-content">
+        <Outlet /> {/*Exibindo conteúdo das páginas filhas de User*/}
+      </main>
     </div>
   )
 }
+
 
 //Exportando página
 export default User

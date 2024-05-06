e11352608afcb8d18bef6231f7a8a849c16520e9
Abner Benicio <abnerbeniciosilva123@gmail.com>
2024-05-06 10:05:35 -0300

    feat: criando página para recuperação de senha

--- src/pages/RecoverPassword.jsx
+++ src/pages/RecoverPassword.jsx
@@ -1,7 +1,86 @@
-const RecoverPassword = () => {
-  return (
-    <div>RecoverPassword</div>
-  )
-}
+import { useEffect, useState } from "react";
+import API from "../api/api";
 
-export default RecoverPassword
+const RecoverPassword = () => {
+  //Definindo variáveis
+  const [usuarios, setUsuarios] = useState("");
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [exibirInfo, setExibirInfo] = useState(false);
+
+  //Buscando usuários
+  useEffect(() => {
+    const fetchApi = async () => {
+      const res = await API.get("user");
+      setUsuarios(res.data);
+    };
+
+    fetchApi();
+  }, []);
+
+  //Validação do formulário
+  const handleSubmit = (e) => {
+    //Evitando atualização da página
+    e.preventDefault();
+    //Limpando senha
+    setSenha("");
+
+    //Procurando usuário correspondente a informaações digitadas
+    usuarios.map((usuario) => {
+      //Verificando se usuário foi encontrado
+      if (nome.toUpperCase() == usuario.name && email == usuario.email) {
+        //Armazenando senha do usuário
+        setSenha(usuario.password);
+      }
+    });
+
+    //Habilitando exibição de informações
+    setExibirInfo(true);
+  };
+
+  //Retorno da página
+  return (
+    <div>
+      <h2>Esqueceu a sua senha? Recupere-a preenchendo o formulario abaixo!</h2> {/*Apresentação da página*/}
+      {/*Formulário para recuperação da senha*/}
+      <form onSubmit={handleSubmit}>
+        <label> {/*Input para nome*/}
+          <input
+            value={nome}
+            name="nome"
+            type="text"
+            placeholder="Digite seu nome"
+            onChange={(e) => setNome(e.target.value)}
+          />
+        </label>
+        <label> {/*Input para email*/}
+          <input
+            value={email}
+            name="nome"
+            type="email"
+            placeholder="Digite seu email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+
+        <button type="submit">Recuperar</button> {/*Botão para submeter formulário*/}
+      </form>
+
+      {/*Exibindo informação
+        - Se usuário foi achado, exibe senha
+        - Se usuário não foi achado, informa ao usuário
+      */}
+      {exibirInfo && (
+        <h4>
+          {senha != ""
+            ? `Sua senha é: ${senha}`
+            : "Não existe usuário com essas informações"}
+        </h4>
+      )}
+    </div>
+  );
+};
+
+//Exportando página
+export default RecoverPassword;

0ace1b0f07dfbf391518187ff2fd804cfc0bec76
Abner Benicio <abnerbeniciosilva123@gmail.com>
2024-05-07 22:05:34 -0300

    fix: corrigindo rotas

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,28 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    // Configuração para lidar com rotas no lado do cliente
     middlewareMode: 'html',
     force: true,
   },
-  root: resolve(import.meta.url.replace('file://', ''), '../src')
+  build: {
+    outDir: 'build',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
 });
+
 
 

6a0ca831eb856ad419c35955cd77fa20ee8db617
Abner Benicio <abnerbeniciosilva123@gmail.com>
2024-05-07 22:27:18 -0300

    fix: corrigindo rotas

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
-import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
-  server: {
-    middlewareMode: 'ssr',
-    // Lida com SPA fallback
-    fs: {
-      strict: false,
-    },
-  },
-});
-
-
-
-
+  plugins: [react()],
+})
